757dbf9a18ac5c40f735201dcd1b8206e6eb15ff
pratiksha0909 <pratikshasalunkhee09@gmail.com>
2024-12-06 23:42:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+function Restart(){
+    window.location="index.html"
+}
+
+let player="o"
+
+
+function cell0(){
+    let d= document.getElementById('cell0')
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+
+
+function cell1(){
+    let d= document.getElementById("cell1")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+
+function cell2(){
+    let d=document.getElementById("cell2")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell3(){
+    let d=document.getElementById("cell3")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell4(){
+    let d=document.getElementById("cell4")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell5(){
+    let d=document.getElementById("cell5")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell6(){
+    let d=document.getElementById("cell6")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell7(){
+    let d=document.getElementById("cell7")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
+function cell8(){
+    let d=document.getElementById("cell8")
+    if(player=="O"){
+        d.innerHTML="O"
+        player="X"
+    }
+    else{
+        d.innerHTML="X"
+        player='O'
+    }
+}
